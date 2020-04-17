4d1065809f7d522e17d189be95676a9e209b8670
Justin Wang <justin.wang91202@gmail.com>
2020-04-16 18:55:43 -0700

    classified token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'NzAwNDQ0NjQxMjAzNDUzOTgz.XpjHYg._rNnO7nF5ZFUUBf9bx7lgd0K8Ek'
+const token = '____________________'
 
 client.once('ready', () => {
 	console.log('Ready!');
