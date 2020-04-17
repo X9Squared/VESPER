92a0eec6fe4c76d9a8cfae684035d6bef3b98189
Justin Wang <justin.wang91202@gmail.com>
2020-04-16 19:08:51 -0700

    command handler mk1

---
+++ commands/diagnostic.js
@@ -0,0 +1,7 @@
+module.exports = {
+	name: 'info',
+	description: 'Dumps all information about the bot. Why not.',
+	execute(message, args) {
+		message.channel.send("I'm a bot. Justo made me. Y'all suck.");
+	},
+};
--- index.js
+++ index.js
@@ -1,17 +1,35 @@
+//Imports and constants
 const Discord = require('discord.js');
+const config = require("./config.json");
+const fs = require('fs');
 const client = new Discord.Client();
-const token = '____________________'
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
 client.on('message', message => {
-  console.log(message.content);
-  if (message.content === '!readycheck') {
-    message.channel.send("Ready");
-  }
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+	}
 });
 
 
-client.login(token);
+client.login(config.token);
