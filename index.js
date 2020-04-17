3fe1f20a3475588d1f2e3cc6b3fa446e0b62621a
Justin Wang <justin.wang91202@gmail.com>
2020-04-16 18:54:13 -0700

    switch from commando to core discord.js

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 const Discord = require('discord.js');
+const client = new Discord.Client();
 const token = 'NzAwNDQ0NjQxMjAzNDUzOTgz.XpjHYg._rNnO7nF5ZFUUBf9bx7lgd0K8Ek'
-const {
-  CommandoClient
-} = require('discord.js-commando');
-const path = require('path');
-
-const client = new CommandoClient({
-  commandPrefix: '!',
-  owner: '198598210367913984',
-});
-
-client.registry
-  .registerDefaultTypes()
-  .registerGroups([
-    ['general', 'General, multipurpose commands'],
-  ])
-  .registerDefaultGroups()
-  .registerDefaultCommands()
-  .registerCommandsIn(path.join(__dirname, 'commands'));
-
 
 client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
-  client.user.setActivity('Super-ultra-hyper-threading');
+	console.log('Ready!');
 });
 
-client.on('error', console.error);
+client.on('message', message => {
+  console.log(message.content);
+  if (message.content === '!readycheck') {
+    message.channel.send("Ready");
+  }
+});
+
 
 client.login(token);
